f6551fffb99e58a0b2b257fbddbae609f290d01b
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-07-07 09:39:29 +0800

    refactor: remove unnesessary import

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <label htmlFor="search">Search</label>
+      <input type="text" id="search" name="search" />
+      <button>Search</button>
+    </div>
+  );
 }
 
 export default App;
