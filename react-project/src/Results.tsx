04ee59b5d6ea2ce1ed93a0ad96b7dc55e73cde0a
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-07-07 11:42:36 +0800

    feat: add results class component

--- react-project/src/Results.tsx
+++ react-project/src/Results.tsx
@@ -1,5 +1,34 @@
-function Results() {
-  return <div></div>;
+import { Component } from "react";
+import { getAnimals } from "./star-wars-data";
+import Card from "./Card";
+
+type ResultsProps = object;
+type ResultsState = object;
+
+export type Animal = {
+  name: string;
+  id: string;
+  description: string;
+};
+
+class Results extends Component<ResultsProps, ResultsState> {
+  state = { animals: [] };
+
+  componentDidMount() {
+    getAnimals().then((animals) => {
+      this.setState({ animals });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.animals.map((animal: Animal) => (
+          <Card key={animal.id} animal={animal}></Card>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Results;
