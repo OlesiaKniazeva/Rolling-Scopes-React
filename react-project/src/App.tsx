c4cebf349e9e7f3662b932e1e4b3232ba35acacb
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-07-07 22:44:43 +0800

    feat: add errorButton and boundary element

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -2,39 +2,52 @@ import { Component } from "react";
 import Results from "./components/Results";
 import Search from "./components/Search";
 import Button from "./components/Button";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./App.css";
 
 type AppProps = object;
 
 type AppState = {
   lastSearchValue: string;
+  errorData: string;
 };
 
 class App extends Component<AppProps, AppState> {
   state = {
     lastSearchValue: "",
+    errorData: "",
   };
 
   setSearchValue = (value: string) => {
     console.log("Update value: ", value);
-    this.setState({ lastSearchValue: value });
+    this.setState({ lastSearchValue: value, errorData: "" });
   };
 
-  generateError() {
-    throw new Error("Something went wrong");
-  }
+  passErrorToResults = (errorData: string) => {
+    this.setState({ errorData });
+  };
 
   render() {
     return (
-      <>
+      <ErrorBoundary>
         <Search handleValueChange={this.setSearchValue} />
-        <Button className="ErrorButton" handleClick={this.generateError}>
+
+        <Button
+          className="ErrorButton"
+          handleClick={() =>
+            this.passErrorToResults("Error data passed from Button component")
+          }
+        >
           Error Button
         </Button>
         <hr />
-        <Results searchValue={this.state.lastSearchValue} />
-      </>
+        <Results
+          searchValue={this.state.lastSearchValue}
+          errorData={this.state.errorData}
+        />
+      </ErrorBoundary>
     );
   }
 }
+
 export default App;
