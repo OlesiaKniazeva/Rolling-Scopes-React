46b3aa6537d60be082566e10ea71631ce4030639
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-07-10 17:22:44 +0800

    refactor: change API and data catching

--- react-project/src/api/api-data.ts
+++ react-project/src/api/api-data.ts
@@ -1,14 +1,14 @@
-async function fetchData(url: string, query: string) {
+async function fetchData(URL: string) {
+  const FETCH_OBJECT_NAME = "docs";
   try {
-    const fullUrl = `${url}?search=${encodeURIComponent(query)}`;
-    const response = await fetch(fullUrl);
+    const response = await fetch(URL);
 
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      console.error("Network response was not ok");
     }
 
     const data = await response.json();
-    return data.results;
+    return data[FETCH_OBJECT_NAME];
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
@@ -16,33 +16,49 @@ async function fetchData(url: string, query: string) {
 }
 
 type DataObject = {
-  name: string;
-  classification: string;
-  language: string;
+  title: string;
+  _version_: string;
+  author_name?: string;
+  cover_i?: string;
+  ratings_average?: number;
+  numberOfPages: number | null;
+  number_of_pages_median?: number;
 };
 
-function formatDescription(obj: DataObject) {
-  if (obj.language === "n/a") {
-    return `${obj.name} is a ${obj.classification.toLowerCase()}.`;
-  }
-
-  return `${obj.name} is a ${obj.classification.toLowerCase()} speaking ${obj.language} language.`;
+function createDescriptionData(obj: DataObject) {
+  return {
+    author: obj.author_name ? obj.author_name : null,
+    cover: obj.cover_i
+      ? `https://covers.openlibrary.org/b/id/${obj.cover_i}-M.jpg`
+      : null,
+    rating: obj.ratings_average ? obj.ratings_average : null,
+    numberOfPages: obj.number_of_pages_median
+      ? obj.number_of_pages_median
+      : null,
+  };
 }
 
 function formatTheData(data: Array<DataObject>) {
   return data.map((obj) => {
     return {
-      name: obj.name,
-      id: crypto.randomUUID().toString(),
-      description: formatDescription(obj),
+      name: obj.title,
+      id: obj._version_,
+      description: createDescriptionData(obj),
     };
   });
 }
 
-export async function getData(searchTerm = "") {
+export async function getData(searchTerm = "", limit = 20, offset = 0) {
   try {
-    const URL = "https://swapi.dev/api/species/";
-    const result = await fetchData(URL, searchTerm);
+    const BASE_URL = "http://openlibrary.org/search.json";
+    let search_settings;
+    if (searchTerm === "") {
+      search_settings = `?q=book&limit=${limit}&offset=${offset}`;
+    } else {
+      search_settings = `?q=book&title=${searchTerm}&limit=${limit}&offset=${offset}`;
+    }
+    const SEARCH_URL = BASE_URL + search_settings;
+    const result = await fetchData(SEARCH_URL);
     return formatTheData(result);
   } catch (error) {
     console.error("Problem with API!", error);
