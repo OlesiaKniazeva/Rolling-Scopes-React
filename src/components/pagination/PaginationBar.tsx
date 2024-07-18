b337eb56ae69d1886c98bfe2ea15b1be7d578bd7
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-07-18 18:00:53 +0800

    fix: change navigational string, add disabled state for next button

--- src/components/pagination/PaginationBar.tsx
+++ src/components/pagination/PaginationBar.tsx
@@ -1,18 +1,26 @@
 import styles from "./PaginationBar.module.css";
-import { NavLink, useSearchParams } from "react-router-dom";
+import { NavLink, useSearchParams, useNavigation } from "react-router-dom";
 function PaginationBar() {
   const [searchParams] = useSearchParams();
+  const navigation = useNavigation();
+  const isSubmitting =
+    navigation.state === "submitting" || navigation.state === "loading";
 
   const currentPage = parseInt(searchParams.get("page") || "1");
   const searchTerm = searchParams.get("q") || "";
+  const totalPages = parseInt(localStorage.getItem("totalPages") || "1");
 
   return (
     <nav className={styles.paginationBar}>
-      <NavLink to={`/search?page=${currentPage - 1}q=${searchTerm}`}>
-        {<button disabled={currentPage === 1}>Previous</button>}
+      <NavLink to={`/search?page=${currentPage - 1}&q=${searchTerm}`}>
+        {<button disabled={currentPage === 1 || isSubmitting}>Previous</button>}
       </NavLink>
-      <NavLink to={`/search?page=${currentPage + 1}q=${searchTerm}`}>
-        {<button>Next</button>}
+      <NavLink to={`/search?page=${currentPage + 1}&q=${searchTerm}`}>
+        {
+          <button disabled={currentPage >= totalPages || isSubmitting}>
+            Next
+          </button>
+        }
       </NavLink>
     </nav>
   );
