702732a2f0fdea43bad863aff14633be64ac9e3a
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-07-18 17:15:16 +0800

    feat: add home navlink

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,14 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 function Header() {
-  return <header>Find the book you love</header>;
+  return (
+    <header className={styles.header}>
+      <NavLink to="/search" className={styles.logo}>
+        StorySearch
+      </NavLink>
+      <h1 className={styles.title}>Find the book you love</h1>
+    </header>
+  );
 }
 
 export default Header;
